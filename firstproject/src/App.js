8f81ad8f0008d2bd874927cf1c263ad415cd9eb7
Rahamansir <153214534+Rahamansir@users.noreply.github.com>
2024-03-24 01:14:31 +0530

    Update App.js

--- firstproject/src/App.js
+++ firstproject/src/App.js
@@ -14,9 +14,7 @@ function App() {
 function user(props) {
   return <div className="user">
     <h3>{props.name}</h3>
-    <p>job:{props.job}</p>
-    <p>location:{props.location}</p>
-
-    
+    <p>job: {props.job}</p>
+    <p>location: {props.location}</p>
   </div>
 }
